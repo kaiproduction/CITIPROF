da7a346d595bafa5ef3a2e499175551b7dfd1463
kaiproduction <97531705+kaiproduction@users.noreply.github.com>
2022-04-06 18:26:35 +0300

    kaistudio

--- public_html/lib/module/sys-css.php
+++
@@ -1,5 +0,0 @@
-<link rel="stylesheet" type="text/css" href="/lib/css/anim.css?v7"/>
-<link rel="stylesheet" type="text/css" href="/lib/css/dark.css?v7"/>
-<link rel="stylesheet" type="text/css" href="/lib/css/main.css?v8"/>
-<link rel="stylesheet" type="text/css" href="/lib/css/debug.css?v53"/>
-<link rel="stylesheet" type="text/css" href="/lib/css/scale.css?v7"/>
